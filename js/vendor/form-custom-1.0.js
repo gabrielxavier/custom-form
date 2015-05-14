9260964d5eef25b9c4cd5466e20853f9ea5b59cb
Gabriel Xavier <gabriel.xavier@a2c.com.br>
2015-05-14 16:45:01 -0300

    Functions em objeto removidas

--- js/vendor/form-custom-1.0.js
+++ js/vendor/form-custom-1.0.js
@@ -1,122 +1,163 @@
 $.customForm = function( selector, settings ){
 
-	var customForm = { selector : selector, defaults : {}, vars: {}, functions : {} };
-
-	customForm.defaults = {}
+	var customForm = this;
+	customForm.selector = selector;
+	customForm.vars = {};
+	customForm.defaults = {
+		autoApply : true
+	}
 	$.extend( customForm.defaults, settings );
 
-	customForm.functions = {
-		init : function( targets ){
-			if(  !$(targets).length ){
+	function init( )
+	{
+		if(  !$(customForm.selector).length ){
 				//Atribui a exceção caso o elemento  não for encontrado.
 				customForm.vars.exception = "Não foi possível encontrar os elementos";
 				//Retorna indicando que o plugin não foi inicializado.
 			}else{
-						$( targets ).each(function(){
+					$( customForm.selector ).each(function(){
 
-							if( $(this).is('select') ){
-								$(this).wrap('<div class="select-custom"></div>').parent().prepend('<span class="value"></span>');
-								$(this).on('change.customForm', customForm.functions._customSelect).each( customForm.functions._customSelect );
-							}
+						if( $(this).is('select') ){
+							$(this).wrap('<div class="select-custom"></div>').parent().prepend('<span class="value"></span>');
+							$(this).on('change.customForm', _customSelect).each( _customSelect );
+						}
 
-							if( $(this).is('input[type="file"]') ){
-								$(this).wrap('<div class="file-custom"></div>').parent().prepend('<span class="value"></span>');
-								$(this).on('change.customForm', customForm.functions._customFile).each( customForm.functions._customFile );
-							}
+						if( $(this).is('input[type="file"]') ){
+							$(this).wrap('<div class="file-custom"></div>').parent().prepend('<span class="value"></span>');
+							$(this).on('change.customForm', _customFile).each( _customFile );
+						}
 
-							if( $(this).is('input[type="checkbox"]') ){
-								$(this).wrap('<div class="checkbox-custom"></div>');
-								$(this).on('click.customForm', customForm.functions._customCheckbox).each( customForm.functions._customCheckbox );
-							}
+						if( $(this).is('input[type="checkbox"]') ){
+							$(this).wrap('<div class="checkbox-custom"></div>');
+							$(this).on('click.customForm', _customCheckbox).each( _customCheckbox );
+						}
 
-							if( $(this).is('input[type="radio"]') ){
-								$(this).wrap('<div class="radio-custom"></div>');
-								$(this).on('click.customForm', customForm.functions._customRadio).each( customForm.functions._customRadio );
-							}
+						if( $(this).is('input[type="radio"]') ){
+							$(this).wrap('<div class="radio-custom"></div>');
+							$(this).on('click.customForm', _customRadio).each( _customRadio );
+						}
+		
+					});
+
+					$( customForm.selector )
+						.on('focus.customForm',  	   _changeState)
+						.on('blur.customForm',   	   _changeState)
+						.on('mouseenter.customForm',   _changeState)
+						.on('mouseleave.customForm',   _changeState);
+			 	}
+
+		return customForm;
+	}
+
+	function _customSelect(event)
+	{
+		$this = event.delegateTarget ? $(event.delegateTarget) : $(this);
 			
-						});
+		$this.parent().find('.value').text( $this.find('option:selected').text() );
+	}
 
-						$( customForm.selector )
-							.on('focus.customForm',  			 customForm.functions._changeState)
-							.on('blur.customForm',   			 customForm.functions._changeState)
-							.on('mouseenter.customForm',   customForm.functions._changeState)
-							.on('mouseleave.customForm',   customForm.functions._changeState);
-				 	}
-
-			return customForm;
-		},
-		destroy: function( targets ){
-
-			$( targets ).each(function(){
-				$this = $(this);
-
-				if( $this.is('select') || $this.is('input[type="file"]') )
-					$this.parent().find('.value').remove();
-
-				$this.unwrap();
-
-			});
-
-			$( targets )
-				.off('focus.customForm')
-				.off('blur.customForm')
-				.off('mouseenter.customForm')
-				.off('mouseleave.customForm')
-				.off('click.customForm')
-				.off('change.customForm');
-
-			return customForm;
-		},
-		apply: function(targets)
-		{
-			customForm.functions.init(targets);
+	function _customFile(event)
+	{
+		$this = event.delegateTarget ? $(event.delegateTarget) : $(this);
 			
-			return customForm;
-		},
-		_customSelect: function(event){
-			$this = event.delegateTarget ? $(event.delegateTarget) : $(this);
-			
-			$this.parent().find('.value').text( $this.find('option:selected').text() );
-		},
-		_customFile: function(event){
-			$this = event.delegateTarget ? $(event.delegateTarget) : $(this);
-			
-			$this.parent().find('.value').text( $this.val() );
-		},
-		_customCheckbox: function(event){
-			$this = event.delegateTarget ? $(event.delegateTarget) : $(this);
+		$this.parent().find('.value').text( $this.val() );
+	}
 
-			if ($this.is(':checked')) {
+	function _customCheckbox(event)
+	{
+		$this = event.delegateTarget ? $(event.delegateTarget) : $(this);
+
+		if ($this.is(':checked')) {
+			$this.parent().addClass('checked');
+		}else{
+			$this.parent().removeClass('checked');
+		}
+	}
+
+	function _customRadio(event)
+	{
+		$(customForm.selector).filter('input[type="radio"]').each(function(){
+			$this = $(this);
+			
+			if ( $this.is(':checked')) {
 				$this.parent().addClass('checked');
 			}else{
 				$this.parent().removeClass('checked');
 			}
-		},
-		_customRadio: function(event){
-			$(customForm.selector).filter('input[type="radio"]').each(function(){
-				$this = $(this);
-				
-				if ( $this.is(':checked')) {
-					$this.parent().addClass('checked');
-				}else{
-					$this.parent().removeClass('checked');
-				}
-			});
-		},
-		_changeState: function(event){
-			if( event.type == "focus" )
-				$(event.delegateTarget).parent().addClass('focus');
-
-			if( event.type == "blur" )
-				$(event.delegateTarget).parent().removeClass('focus');
-
-			if( event.type == 'mouseenter' )
-				$(event.delegateTarget).parent().addClass('hover');
-
-			if( event.type == 'mouseleave' )
-				$(event.delegateTarget).parent().removeClass('hover');
-		}
+		});
 	}
-	//Retorna o retorno da função que inicializa o plugin.
-	return customForm.functions.init( customForm.selector );
+
+	function _changeState(event)
+	{
+		if( event.type == "focus" )
+			$(event.delegateTarget).parent().addClass('focus');
+
+		if( event.type == "blur" )
+			$(event.delegateTarget).parent().removeClass('focus');
+
+		if( event.type == 'mouseenter' )
+			$(event.delegateTarget).parent().addClass('hover');
+
+		if( event.type == 'mouseleave' )
+			$(event.delegateTarget).parent().removeClass('hover');
+	}
+	
+	customForm.destroy = function ()
+	{
+		$(  customForm.selector ).each(function(){
+			$this = $(this);
+
+			if( $this.is('select') || $this.is('input[type="file"]') )
+				$this.parent().find('.value').remove();
+
+			$this.unwrap();
+
+		});
+
+		$(  customForm.selector )
+			.off('focus.customForm')
+			.off('blur.customForm')
+			.off('mouseenter.customForm')
+			.off('mouseleave.customForm')
+			.off('click.customForm')
+			.off('change.customForm');
+
+		return customForm;
+	}
+
+	customForm.refresh = function(){
+		$( customForm.selector ).each(function(){
+
+			if( $(this).is('select') ){
+				$(this).each( _customSelect );
+			}
+
+			if( $(this).is('input[type="file"]') ){
+				$(this).each( _customFile );
+			}
+
+			if( $(this).is('input[type="checkbox"]') ){
+				$(this).each( _customCheckbox );
+			}
+
+			if( $(this).is('input[type="radio"]') ){
+				$(this).each( _customRadio );
+			}
+
+		});
+
+		return customForm;
+	}
+
+	customForm.apply = function(){
+		init();
+
+		return customForm;
+	}
+
+	if( customForm.defaults.autoApply == true ){
+		return init();
+	}else{
+		return customForm;
+	}
 };
